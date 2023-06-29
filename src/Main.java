f17e8f4058ebcf951e0ad68e58d92932c5e85bda
Elena Imyarekova <elena.imyarekova1@gmail.com>
2023-06-29 10:37:24 +0300

    gitignore added

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+
+        int weightInKg = 98;
+        double heightInMeters = 1.87;
+        double index = service.calculate(weightInKg, heightInMeters);
+
+        System.out.println(index);
+
+    }
+
+}
